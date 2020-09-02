0c8dda577d8b2d142bc6365311ab23f62a675b5f
Kiki <kiki.wow.z@gmail.com>
2020-09-02 04:38:11 +0200

    back to the normal version

--- bot_loop3.py
+++ bot_loop3.py
@@ -12,8 +12,5 @@ while True:
         print(f'Quitted on: ---{datetime.datetime.now().strftime("%c")}---')
         break
     params = [sys.executable, 'main_d3.py']
-    # params.extend(sys.argv[1:])
-    # subprocess.call(params,)
-    o, e = subprocess.Popen(['python3.7', 'main_d3.py'], stdout=PIPE, stdin=PIPE, stderr=PIPE).communicate()
-    print(o)
-    print(e)
+    params.extend(sys.argv[1:])
+    subprocess.call(params)
