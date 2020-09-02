1e8ee4044ef0ebe0766096ee5fc3469bf714f18c
Kiki <kiki.wow.z@gmail.com>
2020-09-02 05:16:26 +0200

    NULL

--- bot_loop3.py
+++ bot_loop3.py
@@ -14,7 +14,7 @@ while True:
     #params = [sys.executable, 'main_d3.py']
     #params.extend(sys.argv[1:])
     #subprocess.call(params)
-    if str(os.system("$(ps aux | grep -w '[m]ain_d3.py' | awk '{print $2}')")) == "0":
+    if str(os.system("$(ps aux | grep -w '[m]ain_d3.py' | awk '{print $2}') 2> /dev/null")) == "0":
         os.system('nohup python3.7 -u main_d3.py >> tmp/output.log &')
     else:
         time.sleep(0.5)
