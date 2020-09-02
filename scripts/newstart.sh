2046a59ddad630995e29129485d1e27b9b34e32f
Kiki <kiki.wow.z@gmail.com>
2020-09-02 02:44:03 +0200

    Using the correct path

--- scripts/newstart.sh
+++ scripts/newstart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-out=/home/kiki/botNew/DiscordBot3/tmp/bot_nohup.log
-out2=/home/kiki/botNew/DiscordBot3/tmp/loop_log.log
+out=/home/kiki/botNew/DiscordBotNew/tmp/bot_nohup.log
+out2=/home/kiki/botNew/DiscordBotNew/tmp/loop_log.log
 echo "-----Started at: "$(date '+%Y-%m-%d_%H:%M:%S')"-----" >> ${out}
-nohup python3.7 -u /home/kiki/botNew/DiscordBot3/main_d3.py >> ${out} &
+nohup python3.7 -u /home/kiki/botNew/DiscordBotNew/main_d3.py >> ${out} &
 echo "-----Started at: "$(date '+%Y-%m-%d_%H:%M:%S')"-----" >> ${out2}
-nohup python3.7 -u /home/kiki/botNew/DiscordBot3/bot_loop3.py >> ${out2} &
+nohup python3.7 -u /home/kiki/botNew/DiscordBotNew/bot_loop3.py >> ${out2} &
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-out=/home/kiki/botNew/DiscordBot3/tmp/bot_loop.log
+out=/home/kiki/botNew/DiscordBotNew/tmp/bot_loop.log
 echo "-----Restarted at: "$(date '+%Y-%m-%d_%H:%M:%S')"-----" >> ${out}
-nohup python3.7 -u /home/kiki/botNew/DiscordBot3/bot_loop3.py >> ${out} &
+nohup python3.7 -u /home/kiki/botNew/DiscordBotNew/bot_loop3.py >> ${out} &
