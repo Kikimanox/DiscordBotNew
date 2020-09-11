c22e17ecf7be2946209d6ea45f0eec13d9020af1
Kiki <kiki.wow.z@gmail.com>
2020-09-12 00:24:01 +0200

    shhh

--- models/serversetup.py
+++ models/serversetup.py
@@ -53,7 +53,7 @@ class Logging(BaseModel):
     type = CharField()  # reg, leavejoin, modlog
 
 
-db.drop_tables([Guild, WelcomeMsg, Logging, Webhook])
+# db.drop_tables([Guild, WelcomeMsg, Logging, Webhook])
 db.create_tables([Guild, WelcomeMsg, Logging, Webhook])
 
 
