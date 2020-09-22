6637d4265ba2629e2d6690991002a9324d49e1ef
Kiki <kiki.wow.z@gmail.com>
2020-09-23 01:46:26 +0200

    Added a censor list

--- models/serversetup.py
+++ models/serversetup.py
@@ -58,7 +58,7 @@ class Logging(BaseModel):
 
 
 # db.drop_tables([Guild, WelcomeMsg, Logging, Webhook])
-# db.drop_tables([Guild])
+db.drop_tables([Guild])
 db.create_tables([Guild, WelcomeMsg, Logging, Webhook])
 
 
