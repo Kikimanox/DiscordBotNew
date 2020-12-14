5c181437b97f35898eda9e8887f64346524e310d
Kiki <kiki.wow.z@gmail.com>
2020-12-14 14:27:12 +0100

    Fixes to initial clone script

--- RUN_THIS_AFTER_CLONING.py
+++ RUN_THIS_AFTER_CLONING.py
@@ -1,7 +1,9 @@
+import json
 import os
 import sys
 import shutil
 import fileinput
+
 # from utils.dataIOa import dataIOa
 
 for d in ['data', '_scripts', 'tmp', 'logs', 'logs/error', 'logs/info', 'logs/workers', 'settings']:
@@ -15,6 +17,8 @@ if not os.path.exists('config.json'):
     shutil.copy('config.template', 'config.json')
     print('config.json created, please check the file config.json and '
           'fill in the appropriate fields in!!!!! (DO NOT FORGET TO DO THIS)')
+else:
+    print('config.json already exists, not creaiting a new one.')
 
 skipRenaming = input('For different bot instances, please rename '
                      'the following files, to skip this write "skip", to continue press enter:')
@@ -23,21 +27,16 @@ if not skipRenaming and not str(skipRenaming).lower().strip() == 'skip':
           '"bot_loop3.py", "main_d3.py" or any other variation you have already used, aka. make them UNIQUE')
     b_l = input('Enter the new name for the bot_loop3.py file (example: "the_loop.py"): ')
     m_d = input('Enter the new name for the main_d3.py file (example: "othermain.py"): ')
-    with fileinput.FileInput('scripts/stop.sh', inplace=True, backup='.bak') as file:
-        for line in file:
-            print(line.replace('[m]ain_d3.py', f'[{m_d[:1]}]{m_d[1:]}'), end='')
-    with fileinput.FileInput('scripts/stop.sh', inplace=True, backup='.bak') as file:
-        for line in file:
-            print(line.replace('[b]ot_loop3.py', f'[{b_l[:1]}]{b_l[1:]}'), end='')
+
     with fileinput.FileInput('bot_loop3.py', inplace=True, backup='.bak') as file:
         for line in file:
             print(line.replace('main_d3.py', m_d), end='')
     with fileinput.FileInput('bot_loop3.py', inplace=True, backup='.bak') as file:
         for line in file:
             print(line.replace('[m]ain_d3.py', f'[{m_d[:1]}]{m_d[1:]}'), end='')
-    with fileinput.FileInput('scripts/start.sh', inplace=True, backup='.bak') as file:
-        for line in file:
-            print(line.replace('bot_loop3.py', b_l), end='')
+    # with fileinput.FileInput('scripts/start.sh', inplace=True, backup='.bak') as file:
+    #    for line in file:
+    #        print(line.replace('bot_loop3.py', b_l), end='')
 
     os.rename('main_d3.py', f'{m_d}')
     os.rename('bot_loop3.py', f'{b_l}')
@@ -45,10 +44,14 @@ if not skipRenaming and not str(skipRenaming).lower().strip() == 'skip':
     shutil.copy('config.json', 'config.json.bak')
     remove_lines = ['NEW_MAIN_D', 'NEW_BOT_LOOP']
     with open('config.json.bak') as oldfile, open('config.json', 'w') as newfile:
-        for line in oldfile:
-            if not any(bad_line in line for bad_line in remove_lines):
-                newfile.write(line)
-        newfile.write(f'"NEW_MAIN_D: "{m_d}"\nNEW_BOT_LOOP: "{b_l}"') # TODO: fix this shiz
+        #for line in oldfile:
+        #    if not any(bad_line in line for bad_line in remove_lines):
+        #        newfile.write(line)
+        j = json.loads(oldfile.read())
+        j["NEW_MAIN_D"] = m_d
+        j["NEW_BOT_LOOP"] = b_l
+        newfile.write(json.dumps(j, indent=4, sort_keys=False))
+        # newfile.write(f'"NEW_MAIN_D: "{m_d}"\nNEW_BOT_LOOP: "{b_l}"')  # TO DO: fix this shiz
 
     with open('scripts/start.sh', 'w') as f:
         f.write('#!/bin/bash\n')
@@ -57,6 +60,18 @@ if not skipRenaming and not str(skipRenaming).lower().strip() == 'skip':
         f.write(f'nohup python3.7 -u {os.path.abspath(b_l)} >> ' + '${out} 2>&1 &\n')
         f.write(f'echo "Bot started"')
 
+    with open('scripts/stop.sh', 'w') as f:
+        f.write('#!/bin/bash\n')
+        f.write('kill -9 $(ps aux | grep -w "[b]ot_loop3.py" | awk \'{print $2}\')\n')
+        f.write('kill -9 $(ps aux | grep -w "[m]ain_d3.py" | awk \'{print $2}\')\n')
+        f.write('echo "Stopped"')
+
+    with fileinput.FileInput('scripts/stop.sh', inplace=True, backup='.bak') as file:
+        for line in file:
+            print(line.replace('[m]ain_d3.py', f'[{m_d[:1]}]{m_d[1:]}'), end='')
+    with fileinput.FileInput('scripts/stop.sh', inplace=True, backup='.bak') as file:
+        for line in file:
+            print(line.replace('[b]ot_loop3.py', f'[{b_l[:1]}]{b_l[1:]}'), end='')
 
 # serverDataFile = 'data/servers_data_ids.json'
 # if not os.path.exists(serverDataFile):
@@ -85,4 +100,3 @@ test = os.listdir('.')
 for item in test:
     if item.endswith(".bak"):
         os.remove(item)
-
