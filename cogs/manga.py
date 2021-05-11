aa1ec568e74897bdc79963ec3e2205c6bbaaa5bb
Kikimanox <kiki.wow.z@gmail.com>
2021-05-12 01:34:10 +0200

    update the manga cog with guya's search function

--- cogs/manga.py
+++ cogs/manga.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import random
+import re
 from datetime import datetime
 
 import aiohttp
@@ -167,6 +168,118 @@ class Manga(commands.Cog):
         if hasattr(self.bot, "running_chapters") and event.message_id in self.bot.running_chapters and event.user_id == \
                 self.bot.running_chapters[event.message_id]["user"]:
             await self.chapter_flip(event)
+
+    @commands.command(aliases=['ganti'])
+    async def search(self, ctx, *, text: str):
+        """Text search through guya.moe, defaults to the Oshi no Ko series.
+
+        --guya mobile phone (for searching the kaguya-sama manga)
+        --4koma erika (kaguya-sama 4koma search)
+        --doujin maki (kaguya-sama official doujin search)
+        """
+        series_dict = {
+            'main': 'Oshi-no-Ko',
+            'guya': 'Kaguya-Wants-To-Be-Confessed-To',
+            '4koma': 'We-Want-To-Talk-About-Kaguya',
+            'doujin': 'Kaguya-Wants-To-Be-Confessed-To-Official-Doujin'
+        }
+        if text.strip().startswith('--'):
+            series = text.strip().split('--')[1].split(' ', 1)[0]
+            if series in series_dict:
+                slug = series_dict[series]
+                text = text.replace(f'--{series}', '').strip()
+            else:
+                return await ctx.send(
+                    f"The specified series is not recognized. Options are: `{', '.join(series_dict.keys())}`")
+        else:
+            series = 'main'
+            slug = series_dict[series]
+        match = re.search(r'^([><=])(\d+(?:\.\d+)?)(-)?(\d+(?:\.\d+)?)?\s.+', text)
+        if match:
+            if match.group(3):
+                operand_match = False
+            else:
+                operand_match = True
+            for i in range(1, 5):
+                if match.group(i):
+                    text = text[len(match.group(i)):]
+        search_response = {}
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f"https://guya.moe/api/search_index/{slug}/", data={"searchQuery": text}) as resp:
+                if resp.status == 200:
+                    search_response = await resp.json()
+                else:
+                    return await ctx.send(f"Error: server returned {resp.status}")
+        first_word = next(iter(search_response))
+        final_results = {}
+        for variation, matches in search_response[first_word].items():
+            for chapter, pages in matches.items():
+                chapter_float = float(chapter.replace('-', '.'))
+                if match:
+                    if operand_match:
+                        if match.group(1) == "<" and not chapter_float < float(match.group(2)):
+                            continue
+                        elif match.group(1) == ">" and not chapter_float > float(match.group(2)):
+                            continue
+                        elif match.group(1) == "=" and not chapter_float == float(match.group(2)):
+                            continue
+                    else:
+                        if chapter_float <= float(match.group(2)) or chapter_float >= float(match.group(4)):
+                            continue
+                for page in pages:
+                    for search_word in search_response:
+                        if not any(chapter in search_response[search_word][matched_word] and page in
+                                   search_response[search_word][matched_word][chapter] for matched_word, matched_chap in
+                                   search_response[search_word].items()):
+                            break
+                    else:
+                        chapter_float = chapter.replace('-', '.')
+                        if chapter_float not in final_results:
+                            final_results[chapter_float] = set([page])
+                        else:
+                            final_results[chapter_float].add(page)
+        if not final_results:
+            return await ctx.send("Search returned no results.")
+        search_desc = ""
+        chapter_count = 0
+        embeds = []
+
+        def create_embed(embed_page_numb):
+            em = Embed(
+                title=f"Manga text search for \"{text.strip()}\" in {series} series | Page {embed_page_numb} of ",
+                color=0xea7938)
+            em.set_thumbnail(url="https://i.imgur.com/PexT2dz.png")
+            em.set_footer(text="Search powered by https://guya.moe")
+            return em
+
+        for chapter in sorted(final_results, key=float):
+            chapter_count += 1
+            em = create_embed(len(embeds) + 1)
+            this_chapter = f"Ch. {chapter} | Pages: "
+            if series == 'main':
+                this_chapter += ", ".join([f"[{p}](https://guya.moe/reader/series/Oshi-no-Ko/{chapter.replace('.', '-')}/{p})" for p in
+                                           sorted(final_results[chapter])])  # Well this was fun to learn to do
+            else:
+                this_chapter += ", ".join(
+                    [f"[{p}](https://guya.moe/read/manga/{series_dict[series]}/{chapter.replace('.', '-')}/{p})" for p
+                     in sorted(final_results[chapter])])
+            if len(search_desc) + len(this_chapter) <= 2048 and chapter_count < 10:
+                search_desc += f"{this_chapter}\n"
+            else:
+                em.description = search_desc
+                embeds.append(em)
+                em = create_embed(len(embeds) + 1)
+                search_desc = f"{this_chapter}\n"
+                chapter_count = 0
+        em.description = search_desc
+        embeds.append(em)
+        if len(embeds) > 1:
+            for em in embeds:
+                em.title += str(len(embeds))
+            await dutils.SimplePaginator(extras=embeds).paginate(ctx)
+        else:
+            em.title += "1"
+            await ctx.send(content=None, embed=embeds[0])
 
     # async def if_you_need_loop(self):
     #     await self.bot.wait_until_ready()
