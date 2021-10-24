e08cb0bddf079716827c3a90d93767b2f8bf8481
Kikimanox <kiki.wow.z@gmail.com>
2021-10-24 22:55:04 +0200

    bop

--- cogs/amqmod.py
+++ cogs/amqmod.py
@@ -306,6 +306,16 @@ return ret_7
                         return False
         except:
             print('--Catbox not alive??')
+            import ssl
+
+            try:
+                _create_unverified_https_context = ssl._create_unverified_context
+            except AttributeError:
+                # Legacy Python that doesn't verify HTTPS certificates by default
+                pass
+            else:
+                # Handle target environment that doesn't support HTTPS verification
+                ssl._create_default_https_context = _create_unverified_https_context
             traceback.print_exc()
             return False
 
