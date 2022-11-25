dc4d1ccfaed2194ef48eb5705e334960db4762e7
Kikimanox <--local>
2022-11-26 00:24:11 +0100

    3.7 -> 3.9

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 out=/home/kiki/botNew/DiscordBotNew/tmp/bot_loop.log
 echo "-----Started at: "$(date '+%Y-%m-%d_%H:%M:%S')"-----" >> ${out}
-nohup python3.7 -u /home/kiki/botNew/DiscordBotNew/bot_loop3.py >> ${out} 2>&1 &
+nohup python3.9 -u /home/kiki/botNew/DiscordBotNew/bot_loop3.py >> ${out} 2>&1 &
 echo "Bot started"
