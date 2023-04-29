d2e4e5f25cf4dfd4c5bba57ce138d4203cf90341
Kikimanox <--local>
2023-04-29 19:53:00 +0200

    git pull fix

--- main_d3.py
+++ main_d3.py
@@ -545,11 +545,11 @@ async def restart(ctx, options: str = ""):
             os.rename(bot.config['NEW_BOT_LOOP'], 'bot_loop3.py')
         await ctx.send("Running `git pull`...")
         loop = asyncio.get_event_loop()
-        process = subprocess.Popen("git pull", stdout=subprocess.PIPE, shell=True)
+        process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
         stdout, stderr = await loop.run_in_executor(None, process.communicate)
         if "v" in options:
-            a = stdout.decode(encoding='utf8')
-            b = stderr.decode(encoding='utf8')
+            a = stdout.decode(encoding='utf8') if stdout is not None else ""
+            b = stderr.decode(encoding='utf8') if stderr is not None else ""
             a = f'{a}\n{b}' if b else a
             if len(a) > 1900: a = a[:1900] + "\n\n...Content longer than 1900 chars"
             await ctx.send(f"```{a}```")
