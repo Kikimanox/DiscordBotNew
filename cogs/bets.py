db5700f592d721b52e3c7fc30b7a97f4ef381cd6
RandomArthur <16458204+ArthurKun021@users.noreply.github.com>
2023-05-21 15:00:41 +0800

    added the template for the changing of variables

--- cogs/bets.py
+++ cogs/bets.py
@@ -1,15 +1,21 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
 from discord.ext import commands
+
+if TYPE_CHECKING:
+    from bot import KanaIsTheBest
+    from utils.context import Context
 
 
 class Bets(commands.Cog):
     def __init__(
             self,
-            bot: commands.Bot
+            bot: KanaIsTheBest
     ):
         self.bot = bot
 
     @commands.command()
-    async def ASDFfsdfds(self, ctx, *args):
+    async def ASDFfsdfds(self, ctx: Context, *args):
         """Desc here"""
         await ctx.send("Something")
 
@@ -24,8 +30,8 @@ class Bets(commands.Cog):
 
 
 async def setup(
-        bot: commands.Bot
+        bot: KanaIsTheBest
 ):
     ext = Bets(bot)
-    #bot.running_tasks.append(bot.loop.create_task(ext.if_you_need_loop()))
+    # bot.running_tasks.append(bot.loop.create_task(ext.if_you_need_loop()))
     await bot.add_cog(ext)
