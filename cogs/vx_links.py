7fca672bab96264dc698d2c27014fd6a3341164c
arthur <16458204+ArthurKun021@users.noreply.github.com>
2024-01-06 00:41:06 +0800

    added change the user into webhook message delete the old message

--- cogs/vx_links.py
+++ cogs/vx_links.py
@@ -1,6 +1,7 @@
+from calendar import c
 import re
 
-from discord import Message
+from discord import Message, Webhook, Thread
 from discord.ext import commands
 
 
@@ -10,30 +11,79 @@ class VxLinks(commands.Cog):
             bot: commands.Bot
     ):
         self.bot = bot
+
+
+    async def cog_load(self) -> None:
+        guilds = self.bot.guilds
+        for guild in guilds:
+            channels = guild.text_channels + guild.forums + guild.voice_channels
+            for channel in channels:
+                webhooks = await channel.webhooks()
+                for webhook in webhooks:
+                    if webhook.name == "vxlinks" and webhook.user == self.bot.user:
+                        await webhook.delete()
+
+    async def create_webhook(self, channel) -> Webhook:
+        if isinstance(channel, Thread):
+            channel = channel.parent
+
+        webhooks = await channel.webhooks()
+        for webhook in webhooks:
+            if webhook.name == "vxlinks" and webhook.user == self.bot.user:
+                return webhook
+        return await channel.create_webhook(name="vxlinks")
+
+    async def send_webhook_message(
+            self,
+            channel,
+            replied_message: Message,
+            content: str
+    ):
+        webhook = await self.create_webhook(channel)
+        if isinstance(channel, Thread):
+            await webhook.send(
+                content,
+                username=replied_message.author.display_name,
+                avatar_url=replied_message.author.display_avatar.url,
+                thread= channel,
+                wait=True
+            )
+        else:
+            await webhook.send(
+                content,
+                username=replied_message.author.display_name,
+                avatar_url=replied_message.author.display_avatar.url,
+                wait=True
+            )
+
+        await replied_message.delete()
+
+
+
+
     @commands.Cog.listener()
     async def on_message(
         self,
         msg: Message
     ):
-        if msg.author.bot:
-            return
 
         twitter_url = r'(https?://(?:www\.)?)(twitter|x)\.com'
         if re.search(twitter_url, msg.content):
-            await msg.edit(suppress=True)
             vxtwitter_url = re.sub(twitter_url, r'\1vxtwitter.com', msg.content)
 
-            await msg.reply(
-                f"{vxtwitter_url}",
-                mention_author=False
+            await self.send_webhook_message(
+                msg.channel,
+                msg,
+                vxtwitter_url
             )
         pixiv_url = r'(https?://(?:www\.)?)pixiv\.net'
         if re.search(pixiv_url, msg.content):
-            await msg.edit(suppress=True)
             phixiv_url = re.sub(pixiv_url, r'\1phixiv.net', msg.content)
-            await msg.reply(
-                f"{phixiv_url}",
-                mention_author=False
+
+            await self.send_webhook_message(
+                msg.channel,
+                msg,
+                phixiv_url
             )
 
 
