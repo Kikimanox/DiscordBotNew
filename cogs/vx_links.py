fa27157258852a2f351840460670292192876d08
arthur <16458204+ArthurKun021@users.noreply.github.com>
2024-01-05 23:53:22 +0800

    added auto change of the links

---
+++ cogs/vx_links.py
@@ -0,0 +1,48 @@
+import re
+
+from discord import Message
+from discord.ext import commands
+
+
+class VxLinks(commands.Cog):
+    def __init__(
+            self,
+            bot: commands.Bot
+    ):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(
+        self,
+        msg: Message
+    ):
+        if msg.author.bot:
+            return
+
+        twitter_url = r'(https?://(?:www\.)?)twitter\.com'
+        if re.search(twitter_url, msg.content):
+            await msg.edit(suppress=True)
+            vxtwitter_url = re.sub(twitter_url, r'\1vxtwitter.com', msg.content)
+            await msg.channel.send(
+                f"{vxtwitter_url}"
+            )
+        pixiv_url = r'(https?://(?:www\.)?)pixiv\.net'
+        if re.search(pixiv_url, msg.content):
+            await msg.edit(suppress=True)
+            phixiv_url = re.sub(pixiv_url, r'\1phixiv.net', msg.content)
+            await msg.channel.send(
+                f"{phixiv_url}"
+            )
+
+
+
+
+
+
+
+async def setup(
+        bot: commands.Bot
+):
+    ext = VxLinks(bot)
+    # bot.running_tasks.append(bot.loop.create_task(ext.if_you_need_loop()))
+    await bot.add_cog(ext)
